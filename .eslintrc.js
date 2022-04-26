7c6adaff8506488bcafb1a75503888a61f8c744b
ChanKai <chankai@ChenKai-MacBook-Pro.local>
2022-04-26 14:53:11 +0800

    chore: 引入prettier插件进行代码格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,68 +11,75 @@ module.exports = {
     node: true
   },
   globals: {
-    defineProps: 'readonly',
-    defineEmits: 'readonly',
-    defineExpose: 'readonly',
-    withDefaults: 'readonly',
-    $ref: 'readonly',
-    $computed: 'readonly'
+    defineProps: "readonly",
+    defineEmits: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly",
+    $ref: "readonly",
+    $computed: "readonly"
   },
+  plugins: ["prettier"],
   extends: [
-    'plugin:vue/vue3-strongly-recommended',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    "prettier",
+    "plugin:vue/vue3-strongly-recommended",
+    "@vue/standard",
+    "@vue/typescript/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/member-delimiter-style': ['error',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
       {
         multiline: {
-          delimiter: 'none'
+          delimiter: "none"
         },
         singleline: {
-          delimiter: 'comma'
+          delimiter: "comma"
         }
-      }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    'space-before-function-paren': ["error", {"anonymous": "always", "named": "never", "asyncArrow": "always"}],
-    'vue/array-bracket-spacing': 'error',
-    'vue/arrow-spacing': 'error',
-    'vue/block-spacing': 'error',
-    'vue/brace-style': 'error',
-    'vue/camelcase': 'error',
-    'vue/comma-dangle': 'error',
-    'vue/component-name-in-template-casing': 'error',
-    'vue/eqeqeq': 'error',
-    'vue/key-spacing': 'error',
-    'vue/match-component-file-name': 'error',
-    'vue/object-curly-spacing': 'error',
-    'no-useless-escape': 'off',
-    '@typescript-eslint/no-this-alias': [
-      'error',
-      {
-        allowDestructuring: true, // Allow `const { props, state } = this`; false by default
-        allowedNames: ['self'] // Allow `const self = this`; `[]` by default
       }
     ],
-    'vue/attribute-hyphenation': 'off',
-    'vue/custom-event-name-casing': 'off'
+    "@typescript-eslint/no-explicit-any": "off",
+    "space-before-function-paren": [
+      "error",
+      { anonymous: "always", named: "never", asyncArrow: "always" }
+    ],
+    "vue/array-bracket-spacing": "error",
+    "vue/arrow-spacing": "error",
+    "vue/block-spacing": "error",
+    "vue/brace-style": "error",
+    "vue/camelcase": "error",
+    "vue/comma-dangle": "error",
+    "vue/component-name-in-template-casing": "error",
+    "vue/eqeqeq": "error",
+    "vue/key-spacing": "error",
+    "vue/match-component-file-name": "error",
+    "vue/object-curly-spacing": "error",
+    "no-useless-escape": "off",
+    "@typescript-eslint/no-this-alias": [
+      "error",
+      {
+        allowDestructuring: true, // Allow `const { props, state } = this`; false by default
+        allowedNames: ["self"] // Allow `const self = this`; `[]` by default
+      }
+    ],
+    "vue/attribute-hyphenation": "off",
+    "vue/custom-event-name-casing": "off"
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         jest: true
       }
     }
   ]
-}
+};
